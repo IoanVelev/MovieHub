9a2135d262cfa895b62e41d5fa91b78f0e8dfa2a
Ioan Velev <ioanvelev46@gmail.com>
2024-03-29 16:27:25 +0200

    Install and configure firebase

---
+++ Client/MovieHub/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBTKcyowl899N1eKnm708hU-d71f4JGeuM",
+    authDomain: "moviehub-770ee.firebaseapp.com",
+    projectId: "moviehub-770ee",
+    storageBucket: "moviehub-770ee.appspot.com",
+    messagingSenderId: "1031179144103",
+    appId: "1:1031179144103:web:3a0735ce11c2d0880d8482"
+  };
