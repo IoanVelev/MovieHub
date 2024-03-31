e71d9d80ae883783ce2b703f65f943c4fc18f138
Ioan Velev <ioanvelev46@gmail.com>
2024-03-31 19:42:00 +0300

    Update home view with routing for home path

--- Client/MovieHub/src/app/app.component.ts
+++ Client/MovieHub/src/app/app.component.ts
@@ -6,31 +6,7 @@ import { SharedService } from './shared.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'MovieHub';
-
-  constructor(private service: SharedService){}
-
-  movies: any = [];
-
-  refreshMovies(){
-    this.service.getMovies().subscribe(res => {
-      this.movies = res;
-      console.log(this.movies);
-    });
-  }
+export class AppComponent {
   
-  ngOnInit(): void {
-    this.refreshMovies();
-  }
-
-  addMovie(name: string, year: string, genre: string){
-    this.service.addMovie(name, year, genre);
-  }
-
-
-  deleteMovie(id: string){
-    this.service.deleteMovie(id);
-  }
 
 }
--- Client/MovieHub/src/app/home/home.component.ts
+++ Client/MovieHub/src/app/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  title = 'MovieHub';
 
+  constructor(private service: SharedService){}
+
+  movies: any = [];
+
+  refreshMovies(){
+    this.service.getMovies().subscribe(res => {
+      this.movies = res;
+      console.log(this.movies);
+    });
+  }
+  
+  ngOnInit(): void {
+    this.refreshMovies();
+  }
+
+  addMovie(name: string, year: string, genre: string){
+    this.service.addMovie(name, year, genre);
+  }
+
+
+  deleteMovie(id: string){
+    this.service.deleteMovie(id);
+  }
 }
