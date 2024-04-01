6b39ddea4d247f3b00a9e44aee75c2d9ba2bd02b
Ioan Velev <ioanvelev46@gmail.com>
2024-04-01 18:43:24 +0300

    Update email validation for login form

--- Client/MovieHub/src/app/user/login/login.component.ts
+++ Client/MovieHub/src/app/user/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { EMAIL_DOMAINS } from 'src/app/constants';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +8,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  domains = EMAIL_DOMAINS;
   constructor() {}
 
   login(form: NgForm){
