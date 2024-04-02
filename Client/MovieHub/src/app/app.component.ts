485073fb2b2875e2c7fcb6feffd93ee35f39d3ea
Ioan Velev <ioanvelev46@gmail.com>
2024-04-02 19:48:25 +0300

    Chane navigation bar based on user status

--- Client/MovieHub/src/app/app.component.ts
+++ Client/MovieHub/src/app/app.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { SharedService } from './shared.service';
+import { Component, OnInit, inject } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   
-
 }
