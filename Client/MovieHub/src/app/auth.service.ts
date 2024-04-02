0d7892730f176a4057190df1a218e3a20cd54761
Ioan Velev <ioanvelev46@gmail.com>
2024-04-02 19:00:26 +0300

    Successfully sign in
    Sign in with correct user data

--- Client/MovieHub/src/app/auth.service.ts
+++ Client/MovieHub/src/app/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, updateProfile } from '@angular/fire/auth';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from '@angular/fire/auth';
 import { Observable, from } from 'rxjs';
 
 @Injectable({
@@ -9,9 +9,19 @@ export class AuthService {
   firebaseAuth = inject(Auth);
 
   register(email: string, password: string): Observable<void> {
-    const promise = createUserWithEmailAndPassword(this.firebaseAuth, email, password)
-    .then(res => updateProfile(res.user, {}))
+    const promise = createUserWithEmailAndPassword(
+      this.firebaseAuth,
+      email,
+      password
+    ).then((res) => updateProfile(res.user, {}));
 
     return from(promise);
-}
+  }
+
+  login(email: string, password: string): Observable<void> {
+    const promise = signInWithEmailAndPassword(this.firebaseAuth, email, password)
+    .then(() => {});
+
+    return from(promise)
+  }
 }
--- Client/MovieHub/src/app/user/login/login.component.ts
+++ Client/MovieHub/src/app/user/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth.service';
 import { EMAIL_DOMAINS } from 'src/app/constants';
 
 @Component({
@@ -9,11 +11,23 @@ import { EMAIL_DOMAINS } from 'src/app/constants';
 })
 export class LoginComponent {
   domains = EMAIL_DOMAINS;
-  constructor() {}
+  authService = inject(AuthService);
+  router = inject(Router);
+  errorMessage: string | null = null;
 
-  login(form: NgForm){
+  login(form: NgForm): void {
     if (form.invalid) {
       return
     }
+    
+   this.authService.login(form.value.email, form.value.password)
+   .subscribe({
+    next: () => {
+      this.router.navigate(['/']);
+    },
+    error: (err) => {
+      this.errorMessage = err.code;
+    }
+   });
   }
 }
